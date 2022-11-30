d0be83c1fa7ed485c074854271529abb120c9d1e
Cem GEÇGEL <gecgelcem@outlook.com>
2022-11-30 15:11:30 +0300

    add: Compiling Patterns as Regex

--- core/src/tinam/Main.java
+++ core/src/tinam/Main.java
@@ -2,6 +2,39 @@ package tinam;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    var decimalDigits     = Pattern.range('0', '9');
+    var hexadecimalDigits = Pattern.or(Pattern.range('0', '9'),
+      Pattern.range('a', 'f'), Pattern.range('A', 'F'));
+    var decimal           = numberLiteral(decimalDigits, Pattern.one("eE"));
+    var hexadecimal       =
+      numberLiteral(Pattern.and(Pattern.one("0"), Pattern.one("xX")),
+        hexadecimalDigits, Pattern.one("pP"));
+    System.out.printf("Decimal: %s%nHexadecimal: %s%n", decimal.regex(),
+      hexadecimal.regex());
+  }
+
+  private static Pattern numberLiteral(Pattern suffix, Pattern digit,
+    Pattern exponentLetter) {
+    return Pattern.word(Pattern.and(suffix, number(digit, exponentLetter)));
+  }
+
+  private static Pattern numberLiteral(Pattern digit, Pattern exponentLetter) {
+    return Pattern.word(number(digit, exponentLetter));
+  }
+
+  private static Pattern number(Pattern digit, Pattern exponentLetter) {
+    var     fractionSeparator = Pattern.one(".");
+    Pattern wholeNumber       = plainNumber(digit);
+    return Pattern.and(wholeNumber,
+      Pattern.optional(Pattern.and(fractionSeparator, wholeNumber)),
+      Pattern.optional(
+        Pattern.and(exponentLetter, Pattern.optional(Pattern.one("+-")),
+          plainNumber(Pattern.range('0', '9')))));
+  }
+
+  private static Pattern plainNumber(Pattern digit) {
+    var deliminator = Pattern.one("'");
+    return Pattern.and(digit,
+      Pattern.zeroOrMore(Pattern.or(Pattern.optional(deliminator), digit)));
   }
 }
--- core/src/tinam/Pattern.java
+++ core/src/tinam/Pattern.java
@@ -1,55 +1,246 @@
 package tinam;
 
 import java.util.List;
-import java.util.Optional;
 
 public sealed interface Pattern {
-  record One(String set) implements Pattern {}
-  record Not(String set) implements Pattern {}
-  record Any() implements Pattern {}
-  record All(String characters) implements Pattern {}
-  record Range(char first, char last) implements Pattern {}
-  record Start() implements Pattern {}
-  record End() implements Pattern {}
-  record Or(List<Pattern> alternatives) implements Pattern {}
-  record And(List<Pattern> sequence) implements Pattern {}
-  record Repeat(Pattern repeated, Optional<Integer> minimum,
-    Optional<Integer> maximum) implements Pattern {}
-  record Unmatched(Pattern unmatched) implements Pattern {}
+  record One(String set) implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:[");
+      appendEscapedSet(builder, set);
+      builder.append("])");
+    }
+  }
 
-  static Pattern one(String set) { return new One(set); }
-  static Pattern not(String set) { return new Not(set); }
+  record Not(String set) implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:[");
+      builder.append('^');
+      appendEscapedSet(builder, set);
+      builder.append("])");
+    }
+  }
+
+  record Any() implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:.)");
+    }
+  }
+
+  record All(String characters) implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:");
+      appendEscaped(builder, characters);
+      builder.append(')');
+    }
+  }
+
+  record Range(char first, char last) implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:[");
+      appendEscapedMember(builder, first);
+      builder.append('-');
+      appendEscapedCharacter(builder, last);
+      builder.append("])");
+    }
+  }
+
+  record Start() implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:^)");
+    }
+  }
+
+  record End() implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:$)");
+    }
+  }
+
+  record Or(List<Pattern> alternatives) implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:");
+      alternatives.get(0).regex(builder);
+      for (var i = 1; i < alternatives.size(); i++) {
+        builder.append('|');
+        alternatives.get(i).regex(builder);
+      }
+      builder.append(')');
+    }
+  }
+
+  record And(List<Pattern> sequence) implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:");
+      for (var p : sequence) { p.regex(builder); }
+      builder.append(')');
+    }
+  }
+
+  record BoundedRepeat(Pattern repeated, int minimum, int maximum)
+    implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:");
+      repeated.regex(builder);
+      builder.append('{');
+      builder.append(minimum);
+      builder.append(',');
+      builder.append(maximum);
+      builder.append('}');
+      builder.append(')');
+    }
+  }
+
+  record InfiniteRepeat(Pattern repeated, int minimum) implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:");
+      repeated.regex(builder);
+      builder.append('{');
+      builder.append(minimum);
+      builder.append(',');
+      builder.append('}');
+      builder.append(')');
+    }
+  }
+
+  record Word(Pattern pattern) implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:");
+      builder.append("\\b");
+      pattern.regex(builder);
+      builder.append("\\b");
+      builder.append(')');
+    }
+  }
+
+  record Unmatched(Pattern unmatched) implements Pattern {
+    @Override public void regex(StringBuilder builder) {
+      builder.append("(?:");
+      builder.append("(?=");
+      unmatched.regex(builder);
+      builder.append(')');
+      builder.append(')');
+    }
+  }
+
+  static Pattern one(String set) {
+    validateSet(set);
+    return new One(set);
+  }
+
+  static Pattern not(String set) {
+    validateSet(set);
+    return new Not(set);
+  }
+
   static Pattern any() { return new Any(); }
+
   static Pattern all(String characters) { return new All(characters); }
+
   static Pattern range(char first, char last) { return new Range(first, last); }
+
   static Pattern start() { return new Start(); }
+
   static Pattern end() { return new End(); }
+
   static Pattern or(Pattern... alternatives) {
+    if (alternatives.length < 2)
+      throw new RuntimeException("There must be at least 2 alternatives!");
     return new Or(List.of(alternatives));
   }
-  static Pattern and(Pattern... sequence) { return new And(List.of(sequence)); }
-  static Pattern optional(Pattern pattern) {
-    return new Repeat(pattern, Optional.empty(), Optional.of(1));
+
+  static Pattern and(Pattern... sequence) {
+    if (sequence.length < 2)
+      throw new RuntimeException("There must be at least 2 in sequence!");
+    return new And(List.of(sequence));
   }
+
   static Pattern zeroOrMore(Pattern repeated) {
-    return new Repeat(repeated, Optional.empty(), Optional.empty());
+    return new InfiniteRepeat(repeated, 0);
   }
   static Pattern oneOrMore(Pattern repeated) {
-    return givenOrMore(repeated, 1);
+    return new InfiniteRepeat(repeated, 1);
   }
-  static Pattern givenOrMore(Pattern repeated, Integer minimum) {
-    return new Repeat(repeated, Optional.of(minimum), Optional.empty());
+  static Pattern givenOrMore(Pattern repeated, int minimum) {
+    return new InfiniteRepeat(repeated, minimum);
   }
-  static Pattern givenOrLess(Pattern repeated, Integer maximum) {
-    return new Repeat(repeated, Optional.empty(), Optional.of(maximum));
+
+  static Pattern optional(Pattern pattern) {
+    return new BoundedRepeat(pattern, 0, 1);
   }
-  static Pattern fixedTimes(Pattern repeated, Integer times) {
-    return repeat(repeated, times, times);
+  static Pattern givenOrLess(Pattern repeated, int maximum) {
+    if (maximum < 1) throw new RuntimeException(
+      "Given or less upper bound must be at least 1!");
+    return new BoundedRepeat(repeated, 0, maximum);
   }
-  static Pattern repeat(Pattern repeated, Integer minimum, Integer maximum) {
-    return new Repeat(repeated, Optional.of(minimum), Optional.of(maximum));
+  static Pattern fixedTimes(Pattern repeated, int times) {
+    if (times < 2)
+      throw new RuntimeException("Fixed repeat must be at least 2 times!");
+    return new BoundedRepeat(repeated, times, times);
   }
+  static Pattern repeat(Pattern repeated, int minimum, int maximum) {
+    if (maximum < minimum) throw new RuntimeException(
+      "Repeat upper bound is less than the lower bound!");
+    if (minimum == maximum && minimum < 2)
+      throw new RuntimeException("Fixed repeat must be at least 2 times!");
+    if (maximum < 1)
+      throw new RuntimeException("Repeat upper bound must be at least 1!");
+    return new BoundedRepeat(repeated, minimum, maximum);
+  }
+
+  static Pattern word(Pattern pattern) { return new Word(pattern); }
+
   static Pattern unmatched(Pattern unmatched) {
     return new Unmatched(unmatched);
+  }
+
+  static void validateSet(String set) {
+    if (set.length() == 0) throw new RuntimeException("Set is empty!");
+    for (var i = 0; i < set.length(); i++) {
+      var c         = set.charAt(i);
+      int lastIndex = set.lastIndexOf(c);
+      if (lastIndex != i) throw new RuntimeException(
+        "Repeating '%c' character in set at indices [%d] and [%d]!".formatted(c,
+          i, lastIndex));
+    }
+  }
+
+  static void appendEscapedSet(StringBuilder builder, String set) {
+    for (var member : set.toCharArray()) {
+      appendEscapedMember(builder, member);
+    }
+  }
+
+  static void appendEscapedMember(StringBuilder builder, char member) {
+    switch (member) {
+    case '\\', '^', '[', ']', '-':
+      builder.append('\\');
+      //$FALL-THROUGH$
+    default:
+      builder.append(member);
+    }
+  }
+
+  static void appendEscaped(StringBuilder builder, String characters) {
+    for (var character : characters.toCharArray()) {
+      appendEscapedCharacter(builder, character);
+    }
+  }
+
+  static void appendEscapedCharacter(StringBuilder builder, char character) {
+    switch (character) {
+    case '\\', '^', '$', '[', ']', '(', ')', '{', '}', '.', '+', '*', '?', '!':
+      builder.append('\\');
+      //$FALL-THROUGH$
+    default:
+      builder.append(character);
+    }
+  }
+
+  void regex(StringBuilder builder);
+
+  default String regex() {
+    var builder = new StringBuilder();
+    regex(builder);
+    return builder.toString();
   }
 }
