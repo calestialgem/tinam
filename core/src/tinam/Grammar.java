7b70ba006637afc99d2cb93333b1755dcdcc46bf
Cem GEÇGEL <gecgelcem@outlook.com>
2022-12-01 17:06:41 +0300

    fix: Grammar With Custom Rule List

--- core/src/tinam/Grammar.java
+++ core/src/tinam/Grammar.java
@@ -1,17 +1,26 @@
 package tinam;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
-public record Grammar(String name, String scopeName, Rule.Combined topLevel) {
+public record Grammar(String name, String scopeName, List<Rule> topLevel) {
   public static Grammar combined(String name, String scopeName,
     Rule... combined) {
-    return new Grammar(name, scopeName,
-      (Rule.Combined) Rule.combined(combined));
+    return combined(name, scopeName, List.of(combined));
   }
   public static Grammar combined(String name, String scopeName,
-    Collection<Rule> combined) {
-    return new Grammar(name, scopeName,
-      (Rule.Combined) Rule.combined(combined));
+    Collection<Rule> set) {
+    var list = new ArrayList<Rule>();
+    for (var member : set) {
+      if (member instanceof Rule.Combined nested && nested.name().isEmpty()) {
+        list.addAll(nested.set());
+      } else {
+        list.add(member);
+      }
+    }
+    return new Grammar(name, scopeName, Collections.unmodifiableList(list));
   }
 
   public void textmate(StringBuilder builder) {
@@ -19,13 +28,17 @@ public record Grammar(String name, String scopeName, Rule.Combined topLevel) {
     appendMapping(builder, "name", name);
     builder.append(',');
     appendMapping(builder, "scopeName", scopeName);
-    builder.append(',');
-    topLevel.list(builder);
-    builder.append(",\"repository\":{");
-    topLevel.set().get(0).define(builder);
-    for (var i = 1; i < topLevel.set().size(); i++) {
+    builder.append(",\"patterns\":[");
+    topLevel.get(0).access(builder);
+    for (var i = 1; i < topLevel.size(); i++) {
       builder.append(',');
-      topLevel.set().get(i).define(builder);
+      topLevel.get(i).access(builder);
+    }
+    builder.append("],\"repository\":{");
+    topLevel.get(0).define(builder);
+    for (var i = 1; i < topLevel.size(); i++) {
+      builder.append(',');
+      topLevel.get(i).define(builder);
     }
     builder.append("}}");
   }
